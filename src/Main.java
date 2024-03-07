98691d26d4f809eaccc24efc1c4c0c7d37b80bcd
davidedileo <dudeda94@gmail.com>
2024-03-07 18:15:38 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Rettangolo rettangolo = new Rettangolo(40,20);
+        rettangolo.calcolaArea();
+
+        Triangolo triangolo = new Triangolo(30,15);
+        triangolo.calcolaArea();
+
+    }
+}
